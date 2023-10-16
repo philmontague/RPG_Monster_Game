a9d0f27b41a09e0f8d171a873530f52afc954f1b
Phil <pamramfam@gmail.com>
2023-10-16 19:09:14 -0400

    updated logic in fightOrRun() function

--- monsterGame.js
+++ monsterGame.js
@@ -1,10 +1,11 @@
 const readline = require('readline-sync'); 
 
-
-
 const player = { 
     name: '', 
-    life: 100
+    life: 100, 
+    weapon: "Bare Hands", 
+    shield: "None", 
+    armor: "None"
 }
 
 const monsters = ["Thornclaw", "Frostbite", "Gloomfang"];
@@ -24,7 +25,20 @@ function runOrFight() {
     const randomMonster = getRandomMonster(); 
     const action = readline.keyIn(`OH NO, a wild ${randomMonster} appeared! Would you like to [r]un or [f]ight?--> `, { limit: 'rf' });
  
-    
+    if (action === 'r') {
+        if (Math.random() < 2 / 3) { 
+            console.log('Wow, you are fast! You escaped from the monster!'); 
+        } else { 
+            console.log('Oops, you are slow and the monster caught you!'); 
+            if (player.armor === 'Armor') {
+                console.log('Fortunately, your armor softened the blow.'); 
+                player.life -= 10; 
+            } else { 
+                player.life -= 20; 
+            }
+            console.log(`You lost ${player.armor === "Armor" ? 10: 20} life points. Your life is now ${player.life}`); 
+        }
+    }
 }
 
 
